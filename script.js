04b643e3f0e0018f567898b48be991c6f0c05186
Falaye <promiseanuoluwa@gmail.com>
2024-05-20 10:57:46 +0100

    fetch data using giphy api

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiKey = "cj2OQS1tU5ks6fGDqHo2PO1hVOaMKATV"
+const apiUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`
+
+
+const getData = async() => {
+    try {
+        const gifGrid = document.getElementById("gif-box");
+
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+    
+        data.data.map((gif) => {
+            const gifElement = document.createElement("img")
+            gifElement.src = gif.images.fixed_height.url;
+            gifElement.alt = gif.title
+            gifGrid.appendChild(gifElement)
+        })
+
+    } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+getData()
